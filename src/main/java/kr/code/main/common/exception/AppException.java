62b1119594f72c86921b4360b0f0744aaf49350e
timeless98 <timeless98p@gmail.com>
2023-05-08 20:52:32 +0900

    jwt library 추가 댓글 작업 진행중 AppException 추가 login 처리 완 ( 토큰 방식 )

---
+++ src/main/java/kr/code/main/common/exception/AppException.java
@@ -0,0 +1,12 @@
+package kr.code.main.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AppException extends RuntimeException{
+
+    private ErrorCode errorCode;
+    private String message;
+}
